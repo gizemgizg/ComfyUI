a956f962634b1bfe11eb390672f8e28b7f232e92
Gizem Malçok <64318805+gizemgizg@users.noreply.github.com>
2025-02-10 16:01:40 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ComfyUI'yi arka planda başlat
-python main.py --listen 0.0.0.0 --port 8188 &
+python /app/ComfyUI/main.py --listen 0.0.0.0 --port 8188 &
 
 # API'yi başlat
-python app.py 
+python /app/app.py
